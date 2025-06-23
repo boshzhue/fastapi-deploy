00fafb7083bc44f187fbe040a6e8f821aa5ffe35
boshzhue <boshzhue@gmail.com>
2025-06-23 16:05:33 +0700

    update project files for deployment

--- app/database.py
+++ app/database.py
@@ -6,13 +6,8 @@ import os
 
 load_dotenv()  
 
-DATABASE_URL = (
-    f"mysql+pymysql://{os.getenv('DB_USER')}:"
-    f"{os.getenv('DB_PASSWORD')}@"
-    f"{os.getenv('DB_HOST')}:"
-    f"{os.getenv('DB_PORT')}/"
-    f"{os.getenv('DB_NAME')}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
